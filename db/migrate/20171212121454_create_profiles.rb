b01c59a211af661af4697fca3ed2c398f59b0fa3
emilio hasbun <emiliohasbun_12@hotmail.com>
2017-12-12 15:18:25 +0100

    fix profile table

--- db/migrate/20171212121454_create_profiles.rb
+++ db/migrate/20171212121454_create_profiles.rb
@@ -1,8 +1,8 @@
 class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
-      t.boolean :user_namecreator
-
+      t.boolean :creator
+      t.string :user_name
       t.timestamps
     end
   end
