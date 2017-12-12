dbd99f41075c24544cb92df45279259bcffd8298
Martine Eisinga <eisingamartine@gmail.com>
2017-12-12 14:52:49 +0100

    Added set up controllers

--- app/controllers/saved_trips_controller.rb
+++ app/controllers/saved_trips_controller.rb
@@ -1,2 +1,12 @@
 class SavedTripsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+
+  def destroy
+  end
+
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,34 @@
 class TripsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def trip_params
+  end
+
+  def set_profile
+  end
+
+  def set_trip
+  end
+
 end
